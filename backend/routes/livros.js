38f4b2ca34b1b502981f9038d43f69770881178f
renatosantos05 <renatosantossilva308@gmai.com>
2024-11-25 20:12:14 -0300

    Adicionar funcionalidade de delete

--- backend/routes/livros.js
+++ backend/routes/livros.js
@@ -3,4 +3,6 @@ const livroController = require("../controllers/livroController");
 
 router.route("/livros/all").get((req, res)=> livroController.getAll(req,res));
 router.route("/livros").post((req, res)=>livroController.create(req,res));
+router.route("/livros/delete/:id").delete((req, res)=> livroController.delete(req,res));
+
 module.exports = router;
